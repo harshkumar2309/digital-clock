2a822f01082b6133847fffbe9559efca0ed4556b
Harsh Kumar <yadavharsh1201@gmail.com>
2025-04-17 04:39:21 +0530

    add date display feature

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const clock = document.getElementById("clock");
-
+const dateDisplay = document.getElementById("date");
 setInterval(function(){
     let date = new Date();
     clock.innerHTML = date.toLocaleTimeString();
+    dateDisplay.innerHTML = date.toDateString();
 }, 1000)
